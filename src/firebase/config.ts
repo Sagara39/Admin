d68e207021d8603137d495f7f1ce44b158d64898
SAGARA DILSHAN <sagaradilshan369@gmail.com>
2025-10-11 14:53:18 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
   "projectId": "studio-5297353122-b4093",
-  "appId": "1:584058376949:web:96356778393e50523e3518",
-  "apiKey": "AIzaSyCD-1rrt41UoF5xM_wL1-43212sFhF22s-g",
+  "appId": "1:1023794794433:web:47f37b59fe724e6d002940",
+  "apiKey": "AIzaSyC1jeU2f7DkDDe41En9BTqovUy2KdfZxjg",
   "authDomain": "studio-5297353122-b4093.firebaseapp.com",
-  "measurementId": "G-L5V88P7B6P",
+  "measurementId": "",
   "storageBucket": "studio-5297353122-b4093.appspot.com",
-  "messagingSenderId": "584058376949"
+  "messagingSenderId": "1023794794433"
 };
