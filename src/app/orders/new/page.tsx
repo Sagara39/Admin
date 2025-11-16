63581eb76512e2d0a1f2d0c88f33c3e5abeb32b9
Sagara Dilshan <sagara39@example.com>
2025-11-16 22:30:13 +0530

    ui(orders): make search and cart tabs equal width (50/50)

--- src/app/orders/new/page.tsx
+++ src/app/orders/new/page.tsx
@@ -134,8 +134,8 @@ export default function NewOrderPage() {
 
   return (
     <AppShell>
-      <div className="grid gap-6 lg:grid-cols-3">
-        <div className="lg:col-span-1">
+      <div className="grid gap-6 lg:grid-cols-2">
+        <div>
           <Card>
             <CardHeader>
               <CardTitle>Search Medicines</CardTitle>
@@ -174,7 +174,7 @@ export default function NewOrderPage() {
           </Card>
         </div>
 
-  <div className="lg:col-span-2">
+        <div>
           <Card>
             <CardHeader>
               <CardTitle>Cart</CardTitle>
